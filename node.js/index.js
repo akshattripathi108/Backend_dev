6496e23dc96a5df39dc7e071c777144a1f7ef19e
akshattripathi108 <akshattripathi10848@gmail.com>
2026-01-28 11:57:57 +0530

    first commit

---
+++ node.js/index.js
@@ -0,0 +1,8 @@
+const user = require('./user_response');
+
+
+// The callback module is imported and executed automatically when this file runs.const fileHandling = require('./file_handling');
+
+// The file_handling module is imported and executed automatically when this file runs.const task = require('./task');
+
+// The task module is imported but not executed automatically. Its functions can be used as needed.
