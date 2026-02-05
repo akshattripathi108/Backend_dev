cc927f7d5662ea260a4a31a5871930d8636fe881
akshattripathi108 <akshattripathi10848@gmail.com>
2026-02-05 13:37:49 +0530

    Add Express.js classwork

---
+++ classwork/express-js/app express_js/data.js
@@ -0,0 +1,9 @@
+let user_data = {
+    "users": [
+        { "id": 1, "name": "Alice", "email": ""},
+        { "id": 2, "name": "Bob", "email": ""},
+        { "id": 3, "name": "Charlie", "email": ""}
+    ]
+};
+
+module.exports = user_data; 
