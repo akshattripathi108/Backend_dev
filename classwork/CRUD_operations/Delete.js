8eec2cc692edd1c30cbc4c3a66125a42272d1705
akshattripathi108 <akshattripathi10848@gmail.com>
2026-02-11 16:35:10 +0530

    Fix delete student logic to prevent deletion of students with marks greater than 70

--- classwork/CRUD_operations/Delete.js
+++ classwork/CRUD_operations/Delete.js
@@ -5,13 +5,20 @@ app.use(express.json());
 let students = [
     { id: 1, name: 'Rajit', description: 'This is Rajit.', marks: 85 },
     { id: 2, name: 'Sanskar', description: 'This is Sanskar', marks: 90 },
-    { id: 3, name: 'Ayushman', description: 'This is Ayushman', marks: 92 }
+    { id: 3, name: 'Ayushman', description: 'This is Ayushman', marks: 69 }
 ];
 
 app.delete('/students/:id', (req, res) => {
     const studentId = parseInt(req.params.id);
     const studentIndex = students.findIndex(s => s.id === studentId);
-
+    const student_marks = students[studentIndex].marks;
+    if (student_marks > 70) {
+        return res.status(400).json({ message: 'Student with marks greater than 70 cannot be deleted' });
+    }
+    else if (student_marks <= 70) {
+        students.splice(studentIndex, 1);
+        return res.json({ message: 'Student deleted successfully' });
+    }
     if (studentIndex === -1) {
         return res.status(404).json({ message: 'Student not found' });
     }
